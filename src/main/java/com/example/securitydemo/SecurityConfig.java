3ba7ead9eeb0c382c53918c040a58200449cc871
SujitJamdade <jamdadesujit58@gmail.com>
2026-01-20 08:44:52 +0530

    hardcoded user for basic auth

---
+++ src/main/java/com/example/securitydemo/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.example.securitydemo;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests((request) -> request.anyRequest().authenticated());
+       http.sessionManagement((session
+                       -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)));
+//        http.formLogin(withDefaults());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
